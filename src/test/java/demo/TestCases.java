cc4b2eed5c1e81df5bd44959cbd284649674cef7
unknown <tejaswinisruthi183@gmail.com>
2025-11-12 02:09:21 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -10,12 +10,17 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
+    static ChromeDriver driver;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -47,6 +52,74 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+    }
+
+    @Test
+    public void testCase01() throws InterruptedException {
+        System.out.println("testCase01 executed");
+        Wrappers wrap = new Wrappers(driver);
+
+        //Navigate to Google form
+        System.out.println("Step 1: Opening URL!");
+        wrap.openUrl("https://forms.gle/wjPkzeSEk1CM7KgGA");
+
+        // Wait until the page is loaded
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Submit']")));
+
+        // Fill in Crio Learner in the first text box
+        System.out.println("Step 2: Enter Crio Learner Name");
+        wrap.typeText(By.xpath("(//input[@type = 'text'])[1]"), "Crio Learner");
+        Thread.sleep(2000);
+
+        // Answer 'Why are you practicing Automation' with EPOCH time
+        System.out.println("Step 3: Enter the reason with EPOCH time");
+        wrap.typeText(By.xpath("//textarea"), "I want to be the best QA Engineer! "+ Wrappers.getEpochTime());
+        Thread.sleep(2000);
+
+        // Select Radio Button for Test Experience
+        System.out.println("Step 4: Select Years of Experience");
+        List<String> list1 = Arrays.asList("0 - 2");
+        wrap.selectOptions(By.xpath("//span[@class='aDTYNe snByac OvPDhc OIC90c']"),list1);
+        Thread.sleep(2000);
+
+        // Select Check Box for learnt skills
+        System.out.println("Step 5: Select the skills learnt");
+        List<String> list2 = Arrays.asList("Java","Selenium","TestNG");
+        wrap.selectOptions(By.className("eBFwI"),list2);
+        Thread.sleep(2000);
+
+        // Select dropdown for 'How should you be addressed?'
+        System.out.println("Step 6: Chosse what would you like to be called");
+        wrap.click(By.xpath("//div[@role='option']//span[text()='Choose']"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@role='option']//span[text()='Ms']")));
+        wrap.click(By.xpath("//div[@role='option']//span[text()='Ms']"));
+        Thread.sleep(2000);
+
+        // Select Date for seven days ago
+        System.out.println("Step 7: Select the seven days ago date");
+        Wrappers.selectDate(7);
+        Thread.sleep(2000);
+
+        // Select Time
+        System.out.println("Step 8: Set Time");
+        Wrappers.setTime();
+
+        //Submit the form
+        System.out.println("Step 9: Submit the form");
+        wrap.click(By.xpath("//span[text()='Submit']"));
+        Thread.sleep(2000);
+
+        // Wait untill you see response page
+        System.out.println("Step 10: Wait for the page to load");
+        wait.until(ExpectedConditions.urlContains("formResponse"));
+
+        // Print the same message on the console upon successful completion
+        System.out.println("Step 11: Print the Success Message!");
+        System.out.println(driver.findElement(By.className("vHW8K")).getText());
+        Thread.sleep(2000);
+
+        Thread.sleep(5000);
     }
 
     @AfterTest
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,8 +1,12 @@
 package demo.wrappers;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -16,4 +20,80 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    private static ChromeDriver driver;
+
+    public Wrappers(ChromeDriver driver){
+        Wrappers.driver = driver;
+    }
+
+    public void openUrl(String url){
+        driver.get(url);
+    }
+
+    public static void typeText(By locator, String text){
+        try{
+            WebElement txtBox = driver.findElement(locator);
+            txtBox.clear();
+            txtBox.sendKeys(text);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+
+    public static void click(By locator){
+        try{
+            driver.findElement(locator).click();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectOptions(By locator, List<String> lst1){
+        try{
+            List<WebElement> lstItems = driver.findElements(locator);
+            for(WebElement item:lstItems){
+                String text = item.getText();
+                if(lst1.contains(text)){
+                    item.click();
+                }
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectDate(int days){
+        try{
+            LocalDate today = LocalDate.now();
+            LocalDate previousSeven = today.minusDays(days);
+            String formattedDate = previousSeven.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+            WebElement dateInput = driver.findElement(By.cssSelector("input[type='date']"));
+            dateInput.sendKeys(formattedDate);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void setTime(){
+        try{
+            WebElement hr = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
+            hr.sendKeys("07");
+            
+            WebElement min = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
+            min.sendKeys("30");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String getEpochTime(){
+        long epochMillis = System.currentTimeMillis();
+        return String.valueOf(epochMillis);    
+    }
 }
